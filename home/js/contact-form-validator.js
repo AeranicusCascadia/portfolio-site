0a505f523845990303af647f8d685b4a0ce251c3
AeranicusCascadia <35699755+AeranicusCascadia@users.noreply.github.com>
2019-05-17 16:22:57 -0700

    begin style alert box

--- home/js/contact-form-validator.js
+++ home/js/contact-form-validator.js
@@ -1,21 +1,26 @@
 function form_validate() {
+	
+	var form_alert_field = document.getElementById('contact-form-alert');
 	var name = document.getElementById('name');
 	var email = document.getElementById('email');
 	var address = document.getElementById('address');
 
 	if (name.value == '') {
-		alert("Please enter your name.");
+		// alert("Please enter your name.");
+		form_alert_field.textContent = "Please enter your name.";
 		return false;
 		}
 		
 	if (email.value == '') {
-		alert("Please enter your email.");
+		// alert("Please enter your email
+		form_alert_field.textContent = "Please enter your email address.";
 		return false;
 		}	
 		
 		
 	if (address.value == '') {
-		alert("Please enter your address.");
+		// alert("Please enter your address.");
+		form_alert_field.textContent = "Please enter your physical address.";
 		return false;
 		}
 }
