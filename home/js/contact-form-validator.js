21f9ea55adec2c12a0848faf217fbb88a68db6ec
AeranicusCascadia <35699755+AeranicusCascadia@users.noreply.github.com>
2019-05-20 16:22:23 -0700

    working on redirects

--- home/js/contact-form-validator.js
+++ home/js/contact-form-validator.js
@@ -1,21 +1,31 @@
 function form_validate() {
+	
+	var form_alert_field = document.getElementById('contact-form-alert');
 	var name = document.getElementById('name');
 	var email = document.getElementById('email');
+	var contact_message = document.getElementById('contact_message');
 	var address = document.getElementById('address');
 
 	if (name.value == '') {
-		alert("Please enter your name.");
+		// alert("Please enter your name.");
+		form_alert_field.textContent = "Please enter your name.";
 		return false;
 		}
 		
 	if (email.value == '') {
-		alert("Please enter your email.");
+		// alert("Please enter your email
+		form_alert_field.textContent = "Please enter your email address.";
 		return false;
 		}	
 		
-		
-	if (address.value == '') {
-		alert("Please enter your address.");
+	if (contact_message.value == '') {
+		// alert("Please enter a message.");
+		form_alert_field.textContent = "Please enter a message.";
 		return false;
 		}
+		
+	if (address.value != '') {
+		alert("Not so fast, scammer!");
+		return false;
+	}	
 }
--- home/php/contact.php
+++ home/php/contact.php
@@ -4,18 +4,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 $name = trim(filter_input(INPUT_POST,"name",FILTER_SANITIZE_STRING));
 $email = trim(filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL));
-$address = trim(filter_input(INPUT_POST,"address",FILTER_SANITIZE_STRING));
+$details = trim(filter_input(INPUT_POST,"details",FILTER_SANITIZE_SPECIAL_CHARS));
+
 }
 
 foreach ($_POST as $key => $value) {
-	echo $key . ": " . $value . "<br>";
+	if ($key == 'address') {
+		continue;
+		} else {
+			echo $key . ": " . $value . "<br>";
+		}
 	}
 
-echo "<br> Thanks for your message!";
 
 $message = "Just a test message";
 
 mail('contact@cascadia-digital.com', 'This is the subject', $message);
+
+header('Location: '.$_SERVER['HTTP_REFERER'] . '#contact-section-anchor');
+ 
+
 
 ?>
 
